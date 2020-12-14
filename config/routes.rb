12976fb042bb9cc69f1328b852fc8044b096ea61
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-14 12:53:56 +0000

    [Finish] 3-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/new'
-  get 'blogs/edit'
-  get 'blogs/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
 end
