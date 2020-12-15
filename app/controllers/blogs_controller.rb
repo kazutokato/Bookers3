355035366ccdccc6bbdf65c93cad321d65628653
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-15 03:55:34 +0000

    [Finish] 3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,6 +7,9 @@ class BlogsController < ApplicationController
   end
 
   def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
@@ -16,5 +19,10 @@ class BlogsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
